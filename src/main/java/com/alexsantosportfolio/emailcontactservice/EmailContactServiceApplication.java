e7e08560021698aef208f1ac7bdedf6f0fcb3787
Alex Santos <alexjosedosantos@gmail.com>
2025-07-06 22:14:41 -0300

    first commit

---
+++ src/main/java/com/alexsantosportfolio/emailcontactservice/EmailContactServiceApplication.java
@@ -0,0 +1,13 @@
+package com.alexsantosportfolio.emailcontactservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailContactServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailContactServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alexsantosportfolio/emailcontactservice/EmailContactServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexsantosportfolio.emailcontactservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailContactServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
