934980c2b2e5e3edbfa24118a7e1e439dee155ce
Alex Santos <alexjosedosantos@gmail.com>
2025-07-24 20:27:22 -0300

    feat: adiciona arquivo funcional inicial de repository

--- src/main/java/com/alexsantosportfolio/emailcontactservice/repository/EmailRepository.java
+++ src/main/java/com/alexsantosportfolio/emailcontactservice/repository/EmailRepository.java
@@ -2,6 +2,18 @@ package com.alexsantosportfolio.emailcontactservice.repository;
 
 import com.alexsantosportfolio.emailcontactservice.entity.EmailEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
-public interface EmailRepository extends JpaRepository<EmailEntity, Long> {
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface EmailRepository extends JpaRepository<EmailEntity, UUID> {
+
+    @Query("SELECT e FROM EmailEntity e WHERE e.assuntoEmail LIKE %:termo% OR e.mensagemEmail LIKE %:termo%")
+    List<EmailEntity> listaEmailsPorConteudo(@Param("termo") String termo);
+
+
 }
