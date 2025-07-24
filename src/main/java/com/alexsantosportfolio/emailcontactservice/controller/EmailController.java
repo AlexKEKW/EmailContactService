c41d27c703ba6c9b0c9413215367e6b32b518a16
Alex Santos <alexjosedosantos@gmail.com>
2025-07-24 20:25:36 -0300

    feat: adiciona arquivo funcional inicial de controller

--- src/main/java/com/alexsantosportfolio/emailcontactservice/controller/EmailController.java
+++ src/main/java/com/alexsantosportfolio/emailcontactservice/controller/EmailController.java
@@ -1,4 +1,41 @@
 package com.alexsantosportfolio.emailcontactservice.controller;
 
+import com.alexsantosportfolio.emailcontactservice.DTO.EnviarEmailDTO;
+import com.alexsantosportfolio.emailcontactservice.entity.EmailEntity;
+import com.alexsantosportfolio.emailcontactservice.service.EmailService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/contato")
 public class EmailController {
+
+    private final EmailService emailService;
+
+    public EmailController(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> enviarEmail(@RequestBody @Valid EnviarEmailDTO emailDTO) {
+        try {
+            var emailId = emailService.enviarEmail(emailDTO);
+            return ResponseEntity.created(URI.create("/v1/contato/" + emailId.toString())).build();
+        } catch (Exception e) {
+            return ResponseEntity
+                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Erro ao enviar mensagem: " + e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EmailEntity>> listaEmails() {
+        return ResponseEntity.ok(emailService.listaEmails());
+    }
+
 }
